d90ecc94c8bae85d85f6d9e74193279e6d864e77
sitaram <sitaramk7770@gmail.com>
2023-08-19 19:25:58 +0530

    Edited the strings for showing current age

--- Mylon.py
+++ Mylon.py
@@ -3,5 +3,6 @@ name = str(input("Please Enter Your Good Name:"))
 year = int(input("please enter the year you born:"))
 print(year)
 currentage = 2023 - year
-print("you are %d old." % (currentage))
+print("Hello "+name+". you are %d years old." % (currentage))
+
 
