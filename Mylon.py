18c53b490a7ea26686b6bd3f33963b535e9dfb36
sitaram <sitaramk7770@gmail.com>
2023-08-19 19:59:44 +0530

    How many Months Old

--- Mylon.py
+++ Mylon.py
@@ -3,6 +3,7 @@ name = str(input("Please Enter Your Good Name:"))
 year = int(input("please enter the year you born:"))
 print(year)
 currentage = 2023 - year
+months = currentage*12
 print("Hello "+name+". you are %d years old." % (currentage))
-
+print("Hello "+name+". you are %d months old." % (months))
 
