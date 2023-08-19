4cafdbab4c61cb3cfc3ebd74548034477d56948b
sitaram <sitaramk7770@gmail.com>
2023-08-19 19:12:30 +0530

    Year commit

---
+++ Mylon.py
@@ -0,0 +1,7 @@
+#Calculate Persons Age Based on Year Of Birth.
+name = str(input("Please Enter Your Good Name:"))
+year = int(input("please enter the year you born:"))
+print(year)
+currentage = 2023 - year
+print("you are %d old." % (currentage))
+
