a1d6782a129e1977541a8725aeb5748d712fb414
sitaram <sitaramk7770@gmail.com>
2023-08-19 20:04:07 +0530

    Added number of days caluculation

--- Mylon.py
+++ Mylon.py
@@ -4,6 +4,7 @@ year = int(input("please enter the year you born:"))
 print(year)
 currentage = 2023 - year
 months = currentage*12
+days = currentage*365
 print("Hello "+name+". you are %d years old." % (currentage))
 print("Hello "+name+". you are %d months old." % (months))
-
+print("Hello "+name+". you are %d days old." % (days))
